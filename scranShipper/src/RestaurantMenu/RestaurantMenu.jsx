b8dd96a98427e994031b64827843c5cd2def97dd
Elizabeth Hartley <elizabeth.rae.gadsbygmail.com>
2024-03-25 14:55:33 +0000

    initial restaurant menu functionality made

---
+++ scranShipper/src/RestaurantMenu/RestaurantMenu.jsx
@@ -0,0 +1,40 @@
+import {useEffect, useState} from "react";
+const RestaurantMenu = (props) => {
+
+    const [menus, getMenu] = useState([]);
+    useEffect(() => {
+        fetchMenus()
+    }, []);
+
+    const fetchMenus = (id) => {
+        fetch('https://food-delivery-api.dev.io-academy.uk/restaurants/' + id).then(response => {
+            return response.json();
+        }).then((data) => {
+            getMenu(data.foodItems);
+        })
+    }
+
+    const displayMenu = (item) => {
+    return (
+        <div>
+            <h3>{item.foodName}</h3>
+            <p>Calories: {item.calories}</p>
+        {item.foodType && <p>Food Type: {item.foodType}</p>}
+            <div>
+                <p>£{item.price}</p>
+                <div>
+                    <button>+</button>
+                    <p>QUANTITY</p>
+                    <button>-</button>
+                </div>
+            </div>
+    </div>
+    )}
+
+    return (
+        <>
+            {menus.map(displayMenu)}
+    </>
+    )}
+
+export default RestaurantMenu
