36024e79aed39329dc1ccb619fa1e3517497bdd2
Max Hamilton <maxeh987@gmail.com>
2024-03-25 15:43:00 +0000

    fixed more merge conflicts

--- scranShipper/src/RestaurantDisplay/index.jsx
+++ scranShipper/src/RestaurantDisplay/index.jsx
@@ -1,28 +1,27 @@
 
 import {useState} from 'react';
 
-const [hidden, setHidden] = useState(false);
-
 const RestaurantDisplay = (props) => {
+
+    const [hidden, setHidden] = useState(false);
 
     const makeRestaurant = (restaurant) => {
         return (
-        <button className="col-12 p-2 m-2 col-sm-2 btn btn-outline-primary fs-3" onClick={() => {
+        <button className="col-12 p-2 m-2 col-sm-2 btn btn-outline-primary fs-3" key={restaurant.name} onClick={() => {
             setHidden(!hidden)
         }}> {restaurant.name} </button>
         )
     }
     return (
         <>
-            {!hidden && props.restaurantList.map(makeRestaurant)}
-            {hidden && <RestaurantMenu restaurantId={restaurant.id}/>}
-            {hidden && <button onClick={() => setHidden(!hidden)}>Back</button>}
-
             <div className="container">
                 <div className="row g-col-5 justify-content-center">
-                        {props.restaurantList.map(makeRestaurant)}
+                    {!hidden && props.restaurantList.map(makeRestaurant)}
                 </div>
             </div>
+
+            {hidden && <RestaurantMenu restaurantId={restaurant.id}/>}
+            {hidden && <button onClick={() => setHidden(!hidden)}>Back</button>}
         </>
     )
 }
