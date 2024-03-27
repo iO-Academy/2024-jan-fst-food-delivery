546d4671df90f8b3334bef1f06ebb6c19de76e9a
Elizabeth Hartley <elizabeth.rae.gadsbygmail.com>
2024-03-27 16:04:21 +0000

    place order button sends to API

--- scranShipper/src/Basket/index.jsx
+++ scranShipper/src/Basket/index.jsx
@@ -1,7 +1,29 @@
 import BasketItem from '../BasketItem/index.jsx'
 import DisplayFees from "../DisplayFees/index.jsx";
+import {useState, useEffect} from "react";
 
 const Basket = () => {
+
+    const [orderState, setOrderState] = useState([])
+
+       const sendOrder = async () => {
+           const response = await fetch('https://food-delivery-api.dev.io-academy.uk/orders',
+               {
+                   method: 'POST',
+                   body: JSON.stringify({
+                       "items": [{"name": "Steak Bowl", "price": 12.30, "qty": 1}],
+                       "total": 12.30
+                   }), headers: {
+                       'content-type': 'application/json'
+                   }
+               })
+           const orderJson = response.json();
+           setOrderState(orderJson);
+           console.log(orderState)
+       }
+
+useEffect(() => {
+    sendOrder()},[])
 
     return (
         <div className='col-md-3'>
@@ -12,7 +34,7 @@ const Basket = () => {
                 <DisplayFees feeType={'Delivery Fee:'} feePrice={'£0.99'}/>
                 <DisplayFees feeType={'Service Fee:'} feePrice={'£1.50'}/>
                 <DisplayFees feeType={'Total:'} feePrice={'sum'}/>
-                <button className=' col-10 p-2 btn btn-success fs-4'>Place Order</button>
+                <button onClick={sendOrder} className=' col-10 p-2 btn btn-success fs-4'>Place Order</button>
             </div>
         </div>
     )
