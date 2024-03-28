79ef0e0cd6a1d6c6a5d90596fa926b772dc9647b
Izayah Jordan <izayahjordan13@gmail.com>
2024-03-28 11:48:08 +0000

    destructured all props in basketItem

--- scranShipper/src/Basket/BasketItem/index.jsx
+++ scranShipper/src/Basket/BasketItem/index.jsx
@@ -1,17 +1,15 @@
-const BasketItem = (props) => {
-
-    const {updateOrder} = props
+const BasketItem = ({updateOrder, foodItem, itemPrice, order}) => {
 
     return (
         <div className='d-flex align-items-center justify-content-between py-3'>
-            <p className='col-7 m-0 fw-bold'>{props.foodItem}</p>
+            <p className='col-7 m-0 fw-bold'>{foodItem}</p>
             <div className='d-flex gap-2 align-items-center'>
                 <button className='col-1 px-2 w-auto btn btn-primary d-flex m-1 justify-content-center'
-                        onClick={() => updateOrder(props.foodItem, -1, props.itemPrice)}>-
+                        onClick={() => updateOrder(foodItem, -1, itemPrice)}>-
                 </button>
-                <p className='col-1 text-center m-0'>{props.order[props.foodItem]}</p>
+                <p className='col-1 text-center m-0'>{order[foodItem]}</p>
                 <button className='col-1 px-2 btn w-auto btn-primary d-flex m-1 justify-content-center'
-                        onClick={() => updateOrder(props.foodItem, +1, props.itemPrice)}>+
+                        onClick={() => updateOrder(foodItem, +1, itemPrice)}>+
                 </button>
             </div>
         </div>
